c4d8a0b6bf4de520b979075bcffd5b463c3b0ef9
kiowadriver <mitch.riley1772@gmail.com>
2020-04-28 14:59:09 -0500

    add support for multiple items
    Please add support for multiple items

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo ${EITEM} ${EPRICE} > /.eauction
+echo ${EITEM1} ${EPRICE1} >> /.eauction
+echo ${EITEM2} ${EPRICE2} >> /.eauction
+
 sed -ri "s/EUSER/$EUSER/" /.esniper 
 sed -ri "s/EPASS/$EPASS/" /.esniper
 esniper -c /.esniper /.eauction
